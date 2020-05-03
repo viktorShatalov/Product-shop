2e7578e701bb78c5daf7a7d6680d3d22e1341ba6
viktr 666 <49025707+viktr1432@users.noreply.github.com>
2020-05-03 22:32:39 +0300

    ++

---
+++ korzinka/assets/js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function () {
+
+    //  mobile-menu
+
+    jQuery('.burger').click(function () {
+        jQuery('.burger,.navbar').toggleClass('active');
+        jQuery('html,body').toggleClass('lock');
+    })
+
+
+
+})
