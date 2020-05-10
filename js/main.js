4fe4d2ed30c591ed0cba16e65da0ec6e6550c188
viktr 666 <49025707+viktr1432@users.noreply.github.com>
2020-05-11 00:39:48 +0300

    +++

--- js/main.js
+++ js/main.js
@@ -154,5 +154,30 @@ jQuery(document).ready(function () {
         e.preventDefault()
     })
 
+    // cart-item counter
+
+    $(function () {
+
+        (function quantityProducts() {
+            var $quantityArrowMinus = $(".quantity-arrow-minus");
+            var $quantityArrowPlus = $(".quantity-arrow-plus");
+            var $quantityNum = $(".qty");
+
+            $quantityArrowMinus.click(quantityMinus);
+            $quantityArrowPlus.click(quantityPlus);
+
+            function quantityMinus() {
+                if ($quantityNum.val() > 1) {
+                    $quantityNum.val(+$quantityNum.val() - 1);
+                }
+            }
+
+            function quantityPlus() {
+                $quantityNum.val(+$quantityNum.val() + 1);
+            }
+        })();
+
+    });
+
 })
 
